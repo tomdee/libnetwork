3b8bb84d0e8928d29d3f0e8c7a38054f0656c4ad
Tom Denham <tom@tomdee.co.uk>
2015-04-20 15:13:51 -0700

    Trivial typo fix

--- system.go
+++ system.go
@@ -9,7 +9,7 @@ import (
 // Via http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=7b21fddd087678a70ad64afc0f632e0f1071b092
 //
 // We need different setns values for the different platforms and arch
-// We are declaring the macro here because the SETNS syscall does not exist in th stdlib
+// We are declaring the macro here because the SETNS syscall does not exist in the stdlib
 var setNsMap = map[string]uintptr{
 	"linux/386":     346,
 	"linux/amd64":   308,
